aa0bbe4dfaee433903aafbb97f6457729077bb6a
lanseudesu <lantisfernandez29@gmail.com>
2026-09-19 14:23:04 +0800

    skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,24 +3,24 @@ import "./Skills.css";
 function Skills() {
     const skills = [
         {
-            title: "Backend",
-            info: "A big part of my development experience has been building the systems behind applications, particularly APIs, databases, authentication, and server-side logic.",
-            items: ["Spring Boot", "Django", "ExpressJS", "PostgreSQL"]
-        },
-        {
-            title: "Frontend",
-            info: "I enjoy turning ideas into responsive interfaces and connecting them to the underlying backend services to create complete, functional applications.",
-            items: ["React", "JavaScript", "HTML", "CSS"]
-        },
-        {
             title: "Programming",
             info: "Through my Computer Science studies and personal projects, I've worked with different languages while developing my foundation in OOP, algorithms, and problem solving.",
-            items: ["Java", "Python", "C++", "JavaScript"]
+            items: ["Java", "Python", "C", "JavaScript", "HTML", "SQL", "x86 Assembly", "Flutter"]
+        },
+        {
+            title: "Backend",
+            info: "A big part of my development experience has been building the systems behind applications, particularly APIs, databases, authentication, and server-side logic.",
+            items: ["Spring Boot", "Django", "ExpressJS", "PostgreSQL", "MySQL", "Firebase", "Supabase"]
+        },
+        {
+            title: "Frontend & Design",
+            info: "I enjoy turning ideas into responsive interfaces and connecting them to the underlying backend services to create complete, functional applications.",
+            items: ["React", "NextJS", "Tailwind CSS", "CSS", "Figma"]
         },
         {
             title: "Tools & Development Practices",
             info: "I use a range of tools and development practices throughout my projects, from version control and cloud services to team collaboration and Agile development.",
-            items: ["Git", "GitHub", "AWS", "Supabase", "Agile", "Scrum"]
+            items: ["Git", "Vercel", "AWS", "Agile", "Scrum"]
         }
     ];
 
